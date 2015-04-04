7afd96deea38cfeea11566bacbae01a3a6ea14e1
duques_g <gregory.duquesnoy@epitech.eu>
2015-04-04 02:01:15 +0200

    commenting
    	adding commenting to IGui

--- interfaces/IGui.hpp
+++ interfaces/IGui.hpp
@@ -7,9 +7,16 @@ class IGui
 {
   virtual ~IGui() { };
 
+  /* createWindows -1 for failure */
   virtual int createWindows(std::pair<int, int>) = 0;
+
+  /* launchMenu return 0 for success and -1 for failure */
   virtual int launchMenu() = 0;
+
+  /* launchGame return 0 for success and -1 for failure */
   virtual int launchGame(const Snake &) = 0;
+
+  /* guiQuit return 0 for success and -1 for failure */
   virtual int guiQuit() = 0;
 }
 
